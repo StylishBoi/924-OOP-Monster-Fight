65a545e25920d4a99d6511218ff27e76833c57fa
StylishBoi <max@rdh.ch>
2024-09-11 14:15:25 +0200

    Add project files.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+// 924-OOP-Monster_Fight.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
+// Steps for Program: 
+//   1. Set up the races/points for monsters (maybe outside function)
+//   2. Allow player to choose for monster 1 and monster 2
+//   3. Make fight happen (loop while)
